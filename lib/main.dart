f343a6b295e1c5ddf9321c30a302cd4f48d2636a
Bryan <bryan.doan45@gmail.com>
2025-02-20 17:38:24 -0600

    added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:childsafeapp/screens/ble_test_page.dart';
 import 'package:flutter/material.dart';
 import 'screens/home_page.dart';
+import 'package:childsafeapp/navigation_scaffold.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: NavigationScaffold()
     );
   }
 }
+
+
 
---
+++ lib/navigation_scaffold.dart
@@ -0,0 +1,54 @@
+import 'package:childsafeapp/screens/ble_test_page.dart';
+import 'package:childsafeapp/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+class NavigationScaffold extends StatefulWidget{
+  NavigationScaffold({super.key});
+
+  @override
+  State<StatefulWidget> createState() => NavigationState();
+}
+
+class NavigationState extends State<NavigationScaffold> {
+
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.bluetooth),
+            label: 'Bluetooth',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.notifications),
+            icon: Badge(child: Icon(Icons.notifications_outlined)),
+            label: 'Notifications',
+          ),
+        ],
+      ),
+      body:
+          <Widget>[
+            /// Home page
+            MyHomePage(title: "Test"),
+            BluetoothTestPage(title: "Bluetooth"),
+            MyHomePage(title: "Placeholder"),
+          ][currentPageIndex],
+    );
+  }
+}
