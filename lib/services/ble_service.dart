e944de79122fdc8ec691ad7fe36e9c94f9aedc16
Bryan <bryan.doan45@gmail.com>
2025-02-24 13:28:49 -0600

    bluetooth connection logic. Untested, need real device

--- lib/services/ble_service.dart
+++ lib/services/ble_service.dart
@@ -1,5 +1,29 @@
-
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 class BleService {
+  FlutterBluePlus flutterBlue = FlutterBluePlus();
+
+  final Guid targetService = Guid("1a691c51-8c3c-4dd9-afcd-527e77c64be7");
+  final Guid targetCharacteristic = Guid("f8a4bc7a-ca55-4fa5-92a6-228e0e47b749");
+
+  final scanTimeout = Duration(seconds: 30);
+  late BluetoothDevice sensor;
+
+  Future<void> _scanForSensor() {
+    return FlutterBluePlus.startScan(withServices: [targetService], timeout: scanTimeout);
+  }
+
+  void getDevice() async {
+    await _scanForSensor();
+    await for (var results in FlutterBluePlus.scanResults) {
+      for (ScanResult result in results) {
+        sensor = result.device; // Loops should be unneccesary. Maybe find a way to unpack easier
+      }
+    }
+  }
   
+  String getAdvName() {
+    return sensor.advName;
+  }
+
 }
