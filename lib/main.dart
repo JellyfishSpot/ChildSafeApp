a3df0141861df939b34004b8f36834ab10bb9ffb
Bryan <bryan.doan45@gmail.com>
2025-03-25 20:32:49 -0500

    flutter plugin initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:childsafeapp/navigation_scaffold.dart';
 
-void main() {
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> main() async {
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+  AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('app_icon');
+  DarwinInitializationSettings initializationSettingsDarwin = DarwinInitializationSettings();
+  final InitializationSettings initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsDarwin);
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings); // no onDidReceiveNotifications. Might be necessary?
+
   runApp(const MyApp());
 }
 
